f3fa68e0a8fb4f172d55be740866022aff9fcea4
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-03-16 14:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // write js code if required
+#flip {
+  width: 300px; 
+  height: 200px; 
+  background-image: url('https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg');
+  background-size: cover;
+  transition: background-image 0.3s ease; 
+}
+#flip:hover {
+  background-image: url('https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg');
+}
